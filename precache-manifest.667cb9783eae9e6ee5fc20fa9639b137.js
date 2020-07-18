6984e96e4e697adea71d574957bc379d44a2f694
Sebastian Waisbrot <seppo0010@gmail.com>
2020-07-18 09:13:20 -0300

    Updates

---
+++ precache-manifest.667cb9783eae9e6ee5fc20fa9639b137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf774901dc36d3123e2d35610da6b744",
+    "url": "/sysarmy-sueldos-2020.1/index.html"
+  },
+  {
+    "revision": "4202ce0b64aa24c308c4",
+    "url": "/sysarmy-sueldos-2020.1/static/css/main.69ab941d.chunk.css"
+  },
+  {
+    "revision": "5f6e5c3b476177868a27",
+    "url": "/sysarmy-sueldos-2020.1/static/js/2.71ddf853.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sysarmy-sueldos-2020.1/static/js/2.71ddf853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4202ce0b64aa24c308c4",
+    "url": "/sysarmy-sueldos-2020.1/static/js/main.5e2e36ff.chunk.js"
+  },
+  {
+    "revision": "dd260f44764cb170a11c",
+    "url": "/sysarmy-sueldos-2020.1/static/js/runtime-main.1415fd7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50dab21d2fb25889c26ab61091a3404b.js"
+  "/sysarmy-sueldos-2020.1/precache-manifest.667cb9783eae9e6ee5fc20fa9639b137.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sysarmy-sueldos-2020.1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
